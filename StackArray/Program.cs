0ba65a13812dfb48a515fc1e99116df4c25f85c8
zeynepyildiz1 <azeynepyildiz1@gmail.com>
2021-06-07 13:04:26 +0300

    StackArray

---
+++ StackArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace StackArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push("Elma");
+            stack.Push("Armut");
+            stack.Push("Erik");
+            //Example 1
+            Stack[] array = new Stack[3];
+            array[0] = stack;
+            array[1] = stack;
+            array[2] = stack;
+
+            //Example 2
+            Stack[] array2 = { stack, stack, stack };
+            foreach (var arr in array2)
+            {
+                foreach (var item in arr)
+                {
+                    Console.WriteLine(item);
+
+                }
+
+            }
+        }
+    }
+}
